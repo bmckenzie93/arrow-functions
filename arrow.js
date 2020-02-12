df6382cc0142857d8dea44c00edd76e5a6117424
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2020-02-11 21:03:26 -0800

    Exercises completed

---
+++ arrow.js
@@ -0,0 +1,29 @@
+// const sayHello = function() {
+//   console.log('Hello');
+// }
+
+// const sayHello = () => {
+//   console.log('Hello');
+// }
+
+// One line function does not need braces
+// const sayHello = () => console.log('Hello');
+
+// One line return
+// const sayHello = () => 'hello';
+
+// Return Object - needs parenthesis
+// const returnObject = () => ({ message: 'hello' });
+
+
+// Single param does not need parenthesis
+// const sayHello = name => console.log(`Hello ${name}`);
+
+// Multiple param do need parenthesis
+// const sayHello = (firstName, lastName) => console.log(`Hello ${firstName} ${lastName}`);
+
+// sayHello('brandon', 'M');
+
+// const array = ['brandon', 'jasira', 'akili'];
+
+// const list = array.forEach(name => console.log(name));
